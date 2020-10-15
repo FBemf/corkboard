e59f27aaeb15c56e11563b3c0e1500f60d84fefd
Felipe Bemfica <felipe@bemfica.dev>
2020-10-15 01:15:48 -0400

    Added go.sum, improved -help

--- main.go
+++ main.go
@@ -88,14 +88,14 @@ func main() {
 func parseArgs() Config {
 	config := Config{}
 	flag.StringVar(&config.databasePath, "db-path", "./notes.db", "Path to the sqlite db.")
-	credentialFile := flag.String("creds-file", "", "Path to a file holding login credentials in the form \"username:password\". Each line holds a valid set of credentials.")
-	credentials := flag.String("creds", "", "Access credentials in the form \"username:password\".")
+	credentialFile := flag.String("creds-file", "", "Path to a file holding login credentials in the form\n\"username:password\". Each line holds a valid set of credentials.")
+	credentials := flag.String("creds", "", "Access credentials in the form\n\"username:password\".")
 	flag.IntVar(&config.port, "port", 8080, "Port to serve the application on.")
-	noteExpiryTime := flag.Int("note-expiry", 7, "Notes which have not been viewed in this many days will be deleted. If set to zero, notes never expire..")
-	flag.StringVar(&config.templatePath, "template-path", "./templates/", "Path to the directory where html templates are stored.")
-	flag.StringVar(&config.staticPath, "static-path", "./static/", "Path to the directory where static assets are stored.")
-	flag.IntVar(&config.numRecentNotes, "recent-notes", 8, "Display this many recent notes on the main page.")
-	flag.StringVar(&config.notePath, "note-path", "", "Path to the directory where the notes are stored. If this is set, store notes as flat files instead of in a db.")
+	noteExpiryTime := flag.Int("note-expiry", 7, "Notes which have not been viewed in this many days will be deleted.\nIf set to zero, notes never expire.")
+	flag.StringVar(&config.templatePath, "template-path", "./templates/", "Path to the directory where html templates are stored.\n")
+	flag.StringVar(&config.staticPath, "static-path", "./static/", "Path to the directory where static assets are stored.\n")
+	flag.IntVar(&config.numRecentNotes, "recent-notes", 8, "Display this many recent notes on the main page.\n")
+	flag.StringVar(&config.notePath, "note-path", "", "Path to the directory where the notes are stored. If this is set,\nstore notes as flat files instead of in a db.")
 	flag.Parse()
 
 	if *noteExpiryTime < 0 {
